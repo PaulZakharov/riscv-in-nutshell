a929cf4c434c73df9d55187fe64f0f6d63df1c90
George Korepanov <gkorepanov.gk@gmail.com>
2019-03-04 13:03:41 +0300

    Moved ISA to separate file

--- Code/instruction/instruction.cpp
+++ Code/instruction/instruction.cpp
@@ -1,14 +1,3 @@
-const Instruction::ISAEntry Instruction::isa_table[] = {
-    // name        opcode  format,          pointer                      funct3 funct7 
-    { "unknown",   0xFF,   Format::UNKNOWN, &Instruction::execute_unknown, 0x0,   0x0},
-    // R type
-    { "add",       ...,    FORMAT_R,        &Instruction::execute_add,     ...,   ...}, // TODO: fill
-    // I type
-
-    ... // TODO: make huge table here
-};
-
-
 Instruction::Instruction(uint32 bytes, Addr PC):
     instr(bytes),
     PC(PC)
@@ -29,6 +18,7 @@ Instruction::Instruction(uint32 bytes, Addr PC):
     new_PC = PC + 4; // to be modified in execute() for jumps
 }
 
+
 void Instruction::init_format() {
     for ( const auto& entry : isa_table) {
         if (match against isa entry) { // fill in the condition or create some hierarchical decoder (not recommended)
@@ -39,6 +29,7 @@ void Instruction::init_format() {
         }
     }
 }
+
 
 void Instruction::init_R() {
     // TODO: implement, set disasm
@@ -52,6 +43,7 @@ void Instruction::init_R() {
 void Instruction::init_UNKNOWN() {
     // TODO: modify disasm
 }
+
 
 void Instruction::execute() {
     (this->*function)();
