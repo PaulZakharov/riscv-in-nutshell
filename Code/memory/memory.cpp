6fa2aa2959b5f25dd11bc882d674df1f36db95e1
PaulZakharov <paltoszaharov@yandex.ru>
2019-03-05 23:21:52 +0300

    Merge resolved

--- Code/memory/memory.cpp
+++ Code/memory/memory.cpp
@@ -1,13 +1,7 @@
 #include "memory.h"
 #include "../infra/elf/elf.h"
 
-<<<<<<< HEAD
 Memory::Memory(const std::string& executable_filename): start_PC(0), data(MAX_VAL8, 0)
-=======
-Memory::Memory(const std::string& executable_filename) :
-    start_PC(0),
-    data(MAX_VAL32, 0)
->>>>>>> 01a0349a13b7d7a6ccf32685af61ed8af3270bb9
 {
     Elf_loader elf_loader;
     elf_loader.init(executable_filename);
@@ -19,50 +13,35 @@ Addr Memory::get_start_PC() const {
     return start_PC;
 }
 
-<<<<<<< HEAD
-uint8 Memory::load_byte(Addr address) {
-    if (address >= MAX_VAL8) {
-=======
 uint8 Memory::load_byte(Addr address) const {
-    if (address >= MAX_VAL32) {
->>>>>>> 01a0349a13b7d7a6ccf32685af61ed8af3270bb9
+    /*if (address >= MAX_VAL8) {
         status = status | 1u; //out of range
         return NO_VAL8;
-    }
+    }*/
     return data[address];
 }
 
-<<<<<<< HEAD
-uint16 Memory::load_half(Addr address) {
-    if (address+1 >= MAX_VAL8) {
-=======
-uint16 Memory::load_half(Addr address) const {
-    if (address+1 >= MAX_VAL32) {
->>>>>>> 01a0349a13b7d7a6ccf32685af61ed8af3270bb9
+uint16 Memory::load_half(Addr address) const  {
+    /*if (address+1 >= MAX_VAL8) {
         status = status | 1u; //out of range
         return NO_VAL16;
     }
     if (address % 2 != 0) {
         status = status | 2u; //misaligned load
         return NO_VAL16;
-    }
+    }*/
     return data[address] | (data[address+1]<<8);
 }
 
-<<<<<<< HEAD
-uint32 Memory::load_word(Addr address) {
-    if (address+3 >= MAX_VAL8) {
-=======
 uint32 Memory::load_word(Addr address) const {
-    if (address+3 >= MAX_VAL32) {
->>>>>>> 01a0349a13b7d7a6ccf32685af61ed8af3270bb9
+    /*if (address+3 >= MAX_VAL8) {
         status = status | 1u; //out of range
         return NO_VAL32;
     }
     if (address % 4 != 0) {
         status = status | 2u; //misaligned load
         return NO_VAL32;
-    }
+    }*/
     return data[address] | data[address+1]<<8 | data[address+2]<<16 | data[address+3]<<24;
 }
 
