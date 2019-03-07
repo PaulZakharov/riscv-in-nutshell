5cd337b5bc37a2a133fc04459caf541861a754a8
George Korepanov <gkorepanov.gk@gmail.com>
2019-03-07 21:13:05 +0300

    Cleaned up

--- Code/instruction/instruction.h
+++ Code/instruction/instruction.h
@@ -8,28 +8,6 @@ class Instruction {
             UNKNOWN
         } format = Format::UNKNOWN;
 
-        union _instr {
-            struct {
-                unsigned funct7  :7;
-                unsigned rs2     :5;
-                unsigned rs1     :5;
-                unsigned funct3  :3;
-                unsigned rd      :5;
-                unsigned opcode  :7;
-            } asR;
-            struct asI {...};  // TODO: fill
-            struct asS {...};  // TODO: fill
-            struct asB {...};  // TODO: fill
-            struct asU {...};  // TODO: fill
-            struct asJ {...};  // TODO: fill
-            uint32 raw {...};  // TODO: fill
-
-            _instr(uint32 bytes) :
-            raw(bytes) {}
-        } instr = {};
-
-        // TODO: create register number to ABI name mapping
-
         using Executor = void (Instruction::*)(void);
 
         struct ISAEntry // TODO: fill with needed fields
@@ -45,9 +23,9 @@ class Instruction {
         static const ISAEntry isa_table[];
 
         // registers
-        Register rs1 = Register::X0;
-        Register rs2 = Register::X0;
-        Register rd = Register::X0;
+        Register rs1 = Register::zero;
+        Register rs2 = Register::zero;
+        Register rd = Register::zero;
 
         // values
         uint32 imm_v = NO_VAL32;
@@ -57,16 +35,9 @@ class Instruction {
 
         bool complete = false;
         const Addr PC = NO_VAL32;
-        const Addr new_PC = NO_VAL32;
 
-        std::string name = "INVALID"
-        std::string disasm = "INVALID <args>";
-
-        void init_format();
-        void init_R();
-        void init_I();
-        void init_J();
-        void init_UNKNOWN();
+        std::string name = "INVALID";
+        std::string disasm = "INVALID";
 
         void execute_add();
         void execute_or();
@@ -99,12 +70,10 @@ class Instruction {
         uint32 get_rs2_v() const { return rs2_v;  }
         uint32 get_rd_v()  const { return rd_v;   }
 
-        Addr get_new_PC()  const { return new_PC; }
         Addr get_PC()      const { return PC;     }
+        Addr get_new_PC()  const;
 
         void execute();
-
-        Instruction& operator = (const Instruction& other) {}
 };
 
 #endif
