b80d8a43f0b6c4a52ec93e69562f234b5aeed10f
George Korepanov <gkorepanov.gk@gmail.com>
2019-03-07 23:06:45 +0300

    Added executors decalrations

--- Code/instruction/instruction.h
+++ Code/instruction/instruction.h
@@ -2,78 +2,70 @@
 #define INSTRUCTION_H
 
 class Instruction {
-    private:
-        enum class Format {
-            R, I, S, B, U, J,
-            UNKNOWN
-        } format = Format::UNKNOWN;
+private:
+    enum class Format {
+        R, I, S, B, U, J,
+        UNKNOWN
+    } format = Format::UNKNOWN;
 
-        using Executor = void (Instruction::*)(void);
+    // registers
+    Register rs1 = Register::zero;
+    Register rs2 = Register::zero;
+    Register rd = Register::zero;
 
-        struct ISAEntry // TODO: fill with needed fields
-        {
-            std::string name;
-            uint8 opcode;
-            uint8 funct3;
-            uint8 funct7;
-            Format format;
-            Instruction::Executor function;
-        };
+    // values
+    uint32 imm_v = NO_VAL32;
+    uint32 rs1_v = NO_VAL32;
+    uint32 rs2_v = NO_VAL32;
+    uint32 rd_v = NO_VAL32;
 
-        static const ISAEntry isa_table[];
+    // other fields
+    bool complete = false;
+    const Addr PC = NO_VAL32;
 
-        // registers
-        Register rs1 = Register::zero;
-        Register rs2 = Register::zero;
-        Register rd = Register::zero;
+    std::string name = "INVALID";
+    std::string disasm = "INVALID";
 
-        // values
-        uint32 imm_v = NO_VAL32;
-        uint32 rs1_v = NO_VAL32;
-        uint32 rs2_v = NO_VAL32;
-        uint32 rd_v = NO_VAL32;
+    // executors
+    void execute_unknown() { assert(0) };
 
-        bool complete = false;
-        const Addr PC = NO_VAL32;
+#define DECLARE_INSN(name, match, mask) \
+void execute_ ## name ();
 
-        std::string name = "INVALID";
-        std::string disasm = "INVALID";
+#include "opcodes.gen.h"
 
-        void execute_add();
-        void execute_or();
-        void execute_unknown();
+#undef DECLARE_INSN
 
-        // TODO: add tons of executors
+    using Executor = void (Instruction::*)(void);
+    Executor function = &Instruction::execute_unknown;
 
-        Executor function = &Instruction::execute_unknown;
+public:
+    explicit Instruction(uint32 bytes, Addr PC);
+    Instruction(Instruction& instr);
 
-    public:
-        Instruction(uint32 bytes, Addr PC);
-        Instruction(Instruction& instr);
+    const std::string& get_disasm() const { return disasm; }
 
-        const std::string& get_disasm() const { return disasm; }
+    Register get_rs1 const { return rs1; }
+    Register get_rs2 const { return rs2; }
+    Register get_rd  const { return rd;  }
 
-        Register get_rs1 const { return rs1; }
-        Register get_rs2 const { return rs2; }
-        Register get_rd  const { return rd;  }
+    bool is_jump() const;
+    bool is_load() const;
+    bool is_store() const;
+    bool is_nop() const;
 
-        bool is_jump() const;
-        bool is_load() const;
-        bool is_store() const;
-        bool is_nop() const;
+    void set_rs1_v(uint32 value);
+    void set_rs2_v(uint32 value);
+    void set_rd_v(uint32 value);
 
-        void set_rs1_v(uint32 value);
-        void set_rs2_v(uint32 value);
-        void set_rd_v(uint32 value);
+    uint32 get_rs1_v() const { return rs1_v; }
+    uint32 get_rs2_v() const { return rs2_v; }
+    uint32 get_rd_v()  const { return rd_v;  }
 
-        uint32 get_rs1_v() const { return rs1_v;  }
-        uint32 get_rs2_v() const { return rs2_v;  }
-        uint32 get_rd_v()  const { return rd_v;   }
+    Addr get_PC()      const { return PC;     }
+    Addr get_new_PC()  const;  // TODO: need to define logic
 
-        Addr get_PC()      const { return PC;     }
-        Addr get_new_PC()  const;
-
-        void execute();
+    void execute() { this->function() };
 };
 
 #endif
