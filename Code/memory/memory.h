f8ef4d314fcbab2abcdaa56619438c3fe17b86d9
George Korepanov <gkorepanov.gk@gmail.com>
2019-03-12 20:07:16 +0300

    wip

--- Code/memory/memory.h
+++ Code/memory/memory.h
@@ -7,13 +7,13 @@ class Memory {
     private:
         std::vector<uint8> data;
         const Addr start_PC;
-        // control information such as
-        // out of range or misalign exception
-        uint8 status = NO_VAL8;
-    public:
-        Memory(const std::string& executable_filename);
 
-        Addr get_start_PC() const;
+        void load(Instruction& instr) const {
+            instr.set_v_dst(read(instr.get_memory_addr(), instr.get_mem_size()));
+        }   
+        void store(const Instruction& instr) {
+            write(instr.get_v_src2(), instr.get_mem_addr(), instr.get_mem_size());
+        }   
 
         uint8 load_byte(Addr address) const;
         uint16 load_half(Addr address) const;
@@ -22,6 +22,18 @@ class Memory {
         void store_byte(Addr address, uint8 stored_data);
         void store_half(Addr address, uint16 stored_data);
         void store_word(Addr address, uint32 stored_data);
+    public:
+        Memory(const std::string& executable_filename);
+        Addr get_start_PC() const;
+
+
+    void load_store(FuncInstr& instr) {
+        if (instr.is_load())
+            load(instr);
+        else if (instr.is_store())
+            store(instr);
+    }   
+
 };
 
 #endif
