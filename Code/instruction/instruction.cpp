d07fe2915a102a022e689d338503ca71ec04be35
George Korepanov <gkorepanov.gk@gmail.com>
2019-03-12 22:10:03 +0300

    Add memory_size field

--- Code/instruction/instruction.cpp
+++ Code/instruction/instruction.cpp
@@ -1,8 +1,6 @@
 #include "instruction.h"
 #include "../rf/rf.h"
 
-using Format = Instruction::Format;
-
 #define I(name) \
 ISA_entry_generated_ ## name
 
@@ -11,19 +9,7 @@ struct ISAEntryGenerated {
     uint32 match;
     uint32 mask;
     Instruction::Executor function;
-}
-
-struct ISAEntry {
-    ISAEntryGenerated generated_entry;
-    Format format;
-    // put here whatever else is needed
-
-    bool match(uint32 raw) const { 
-        return (raw & this->generated_entry.mask) == this->generated_entry.match;
-    }
 };
-
-
 
 #define DECLARE_INSN(name, match, mask) \
 static const ISAEntryGenerated I(name) = \
@@ -31,19 +17,30 @@ static const ISAEntryGenerated I(name) = \
 #include <riscv.opcode.gen.h>
 #undef DECLARE_INSN
 
+struct ISAEntry {
+    ISAEntryGenerated generated_entry;
+    Format format;
+    size_t memory_size;
+
+    bool match(uint32 raw) const { 
+        return (raw & this->generated_entry.mask) == this->generated_entry.match;
+    }
+};
+
+using Format = Instruction::Format;
 
 static const std::vector<ISAEntry> ISA_table = {
-   { I(lui),  Format::U },
-   { I(srai), Format::I }.
+   { I(lui),  Format::U, 0 },
+   { I(srai), Format::I, 0 }.
    ...
 }
 
 
-ISAEntry find_entry(uint32 raw) {
-    for (auto& x : ISA_table)
+const ISAEntry find_entry(uint32 raw) {
+    for (const auto& x : ISA_table) {
         if (x.match(raw))
             return x;
-    
+    }
     assert(0);
 }
 
@@ -58,15 +55,16 @@ Instruction::Instruction(uint32 bytes, Addr PC) :
     this->format = entry.format;
     this->executor  = entry.generated_entry.function;
     this->imm_v = decoder.get_immediate(entry.format);
+    this->memory_size = entry.memory_size;
 
     // TODO: add field to ISA entry to check whether we need to do this
     this->rs1 = static_cast<Register>(decoder.rs1);
     this->rs2 = static_cast<Register>(decoder.rs2);
-    this->rd = static_cast<Register>(decoder.rd);
+    this->rd  = static_cast<Register>(decoder.rd);
 }
-
 
 void Instruction::execute() {
     (this->*function)();
     this->complete = true;
 }
+
