1ab2449ef5035e240288878c274bf39d42f8746a
George Korepanov <gkorepanov.gk@gmail.com>
2019-03-16 21:00:17 +0300

    Extend test case:

--- Code/tests/instruction.cpp
+++ Code/tests/instruction.cpp
@@ -2,7 +2,11 @@
 #include "instruction/instruction.hpp"
 
 TEST_CASE("Dummy test showing that testing works", "[multi-file:2]") { }
-TEST_CASE("Instrcution class test", "[multi-file:2]") {
+TEST_CASE("ADD", "[multi-file:2]") {
     Instruction i(0b0000000'00010'00001'000'10000'0110011, 13u);
     REQUIRE(i.get_name() == "add");
+    REQUIRE(i.get_rd() == 16);
+    REQUIRE(i.get_rs2() == 2);
+    REQUIRE(i.get_rs1() == 1);
+    REQUIRE(i.get_disasm() == "add ra sp a6 -559038737");
 }
