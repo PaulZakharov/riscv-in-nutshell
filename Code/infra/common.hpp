aa55148d42993b7def2c3b3a88d4d0d9c6f41593
George Korepanov <gkorepanov.gk@gmail.com>
2019-03-16 19:04:04 +0300

    Simulator is now linking

--- Code/infra/common.hpp
+++ Code/infra/common.hpp
@@ -7,10 +7,6 @@
 
 #include "types.hpp"
 
-int32 sign_extend(const int bits, uint32 x) {
-    uint32 m = 1;
-    m <<= bits - 1;
-    return static_cast<int32>((x ^ m) - m);
-}
+int32 sign_extend(const int bits, uint32 x);
 
 #endif
--- Code/rf/register.hpp
+++ Code/rf/register.hpp
@@ -43,20 +43,4 @@ private:
     const std::string get_name() const { return this->names_table[this->id()]; }
 };
 
-const std::array<std::string, Register::MAX_NUMBER> Register::names_table = {
-    "zero",
-    "ra",
-    "sp",
-    "gp",
-    "tp",
-    "t0",
-    "t1", "t2",
-    "s0",
-    "s1",
-    "a0", "a1",
-    "a2", "a3", "a4", "a5", "a6", "a7",
-    "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11",
-    "t3", "t4", "t5", "t6"
-};
-
 #endif
